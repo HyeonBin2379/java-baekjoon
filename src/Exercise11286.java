455c3fc8439d9d1cd822420c40ccef354854f6db
Hyeonbin2379 <hyeonbin2379@gmail.com>
2023-11-28 17:43:53 +0900

    스택, 큐, 덱 관련 문제풀이 소스코드 추가-2

---
+++ src/Exercise11286.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Exercise11286 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int inputCount = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> {
+            int comparison = Math.abs(o1)-Math.abs(o2);
+            return (comparison != 0) ? comparison : o1-o2;
+        });
+
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < inputCount; i++) {
+            int request = Integer.parseInt(br.readLine());
+            if (request == 0) {
+                int result = (queue.isEmpty()) ? 0 : queue.poll();
+                sb.append(result).append("\n");
+            }
+            else {
+                queue.add(request);
+            }
+        }
+        System.out.println(sb);
+        br.close();
+    }
+}
