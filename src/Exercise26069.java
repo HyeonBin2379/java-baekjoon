438297c79032116efb397a504a518769c8662a63
Hyeonbin2379 <hyeonbin2379@gmail.com>
2023-12-10 23:05:06 +0900

    동적 계획법 관련 문제풀이 코드 추가

---
+++ src/Exercise26069.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Exercise26069 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int meetLogCount = Integer.parseInt(br.readLine());
+        Set<String> dance = new HashSet<>();
+        dance.add("ChongChong");
+        for (int i = 0; i < meetLogCount; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String nameA = st.nextToken();
+            String nameB = st.nextToken();
+            if (!dance.contains(nameA) && !dance.contains(nameB)) {
+                continue;
+            }
+            dance.add(nameA);
+            dance.add(nameB);
+        }
+        br.close();
+        System.out.println(dance.size());
+    }
+}
---
+++ src/Exercise9252.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Exercise9252 {
+    private static int[][] dp;
+    private static char[] strA, strB;
+    private static final StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        strA = br.readLine().toCharArray();
+        strB = br.readLine().toCharArray();
+        br.close();
+
+        dp = new int[strA.length+1][strB.length+1];
+        for (int i = 1; i <= strA.length; i++) {
+            for (int j = 1; j <= strB.length; j++) {
+                if (strA[i-1] == strB[j-1]) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        System.out.println(dp[strA.length][strB.length]);
+        getText(strA.length, strB.length);
+        System.out.println(sb.reverse());
+    }
+
+    public static void getText(int row, int col) {
+        if (row == 0 || col == 0) {
+            return;
+        }
+        if (strA[row-1] == strB[col-1]) {
+            sb.append(strA[row-1]);
+            getText(row-1, col-1);
+        } else {
+            if (dp[row-1][col] > dp[row][col-1]) {
+                getText(row-1, col);
+            } else {
+                getText(row, col-1);
+            }
+        }
+    }
+}
