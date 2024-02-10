90d0f209a09753e6e27386f1ab08c6a2c1b9357c
Hyeonbin2379 <hyeonbin2379@gmail.com>
2024-02-10 16:42:07 +0900

    백준 문제풀이 소스코드 추가

---
+++ src/bakjoon/Exercise1094.java
@@ -0,0 +1,29 @@
+package bakjoon;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Exercise1094 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Integer> sticks = new ArrayList<>(List.of(64));
+        int length = sc.nextInt();
+        int sum = sticks.stream().mapToInt(value -> value).sum();
+
+        while (sum != length) {
+            sticks.sort((o1, o2) -> o2 - o1);
+            int newStick = sticks.get(sticks.size()-1) / 2;
+            sticks.set(sticks.size()-1, newStick);
+            if (sum - newStick >= length) {
+                sum -= newStick;
+                continue;
+            }
+            sticks.add(newStick);
+        }
+
+        System.out.println(sticks.size());
+        sc.close();
+    }
+}
