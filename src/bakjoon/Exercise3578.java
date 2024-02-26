c56e55c5fd722950e3ffcc9ee3414f767d9efc2f
Hyeonbin2379 <hyeonbin2379@gmail.com>
2024-02-26 17:36:57 +0900

    백준 문제풀이 소스코드 추가 저장

---
+++ src/bakjoon/Exercise3578.java
@@ -0,0 +1,18 @@
+package bakjoon;
+
+import java.util.Scanner;
+
+public class Exercise3578 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt();
+
+        if (h < 2) {
+            System.out.println((h == 0) ? "1" : "0");
+            return;
+        }
+        String result = (h % 2 == 1) ? "4" : "";
+        result += "8".repeat(h/2);
+        System.out.println(result);
+    }
+}
