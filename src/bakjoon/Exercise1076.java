d9122140897a3d42dd8c040f11b63520f9539e84
Hyeonbin2379 <hyeonbin2379@gmail.com>
2024-02-27 02:22:08 +0900

    백준 문제풀이 소스코드 추가 저장

---
+++ src/bakjoon/Exercise1076.java
@@ -0,0 +1,36 @@
+package bakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Exercise1076 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Map<String, Integer> resistant = new LinkedHashMap<>(Map.of(
+                "black", 0,
+                "brown", 1,
+                "red", 2,
+                "orange", 3,
+                "yellow", 4,
+                "green", 5,
+                "blue", 6,
+                "violet", 7,
+                "grey", 8,
+                "white", 9
+        ));
+        StringBuilder num = new StringBuilder();
+        long result = 0L;
+        for (int i = 0; i < 3; i++) {
+            String color = br.readLine();
+            if (i < 2) {
+                num.append(resistant.get(color).toString());
+            } else {
+                result = Long.parseLong(num.toString()) * (long) Math.pow(10, resistant.get(color));
+            }
+        }
+        System.out.println(result);
+    }
+}
---
+++ src/bakjoon/Exercise4458.java
@@ -0,0 +1,20 @@
+package bakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Exercise4458 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int count = Integer.parseInt(br.readLine());
+        for (int i = 0; i < count; i++) {
+            String input = br.readLine();
+            String firstLetter = String.valueOf(input.charAt(0));
+            input = input.replaceFirst(firstLetter, firstLetter.toUpperCase());
+            sb.append(input).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
