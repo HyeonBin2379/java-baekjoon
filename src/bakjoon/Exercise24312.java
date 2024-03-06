c6284498b155d5273c9126178168a0a8a6224b33
Hyeonbin2379 <hyeonbin2379@gmail.com>
2024-03-06 22:16:24 +0900

    백준 문제풀이 소스코드 추가 저장

---
+++ src/bakjoon/Exercise24312.java
@@ -0,0 +1,20 @@
+package bakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Exercise24312 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] weight = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(weight);
+
+        int sum = Arrays.stream(weight).sum();
+        int diff1 = Math.abs((weight[0]+weight[3] - weight[2] - weight[1]));
+        int diff2 = Math.abs((sum - weight[3]) - weight[3]);
+        System.out.println(Math.min(diff1, diff2));
+    }
+}
