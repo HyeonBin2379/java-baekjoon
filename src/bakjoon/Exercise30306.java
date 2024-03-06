9537ac00e0185216bc8ba4e5cb2cab4f7f8e32e7
Hyeonbin2379 <hyeonbin2379@gmail.com>
2024-03-07 02:11:20 +0900

    백준 문제풀이 소스코드 추가 저장

---
+++ src/bakjoon/Exercise30306.java
@@ -0,0 +1,39 @@
+package bakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Exercise30306 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int len = Integer.parseInt(br.readLine());
+        int[] dice1 = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] dice2 = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int first = 0, second = 0;
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                if (dice1[i] == dice2[j]) {
+                    continue;
+                }
+
+                if (dice1[i] > dice2[j]) {
+                    first++;
+                } else {
+                    second++;
+                }
+            }
+        }
+
+        if (first > second) {
+            System.out.println("first");
+        } else if (first == second) {
+            System.out.println("tie");
+        } else {
+            System.out.println("second");
+        }
+    }
+}
