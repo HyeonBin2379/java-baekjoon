64b12bf2cca47a00f7479d8e99462fc92cb2b2fe
Hyeonbin2379 <hyeonbin2379@gmail.com>
2024-03-10 22:10:16 +0900

    백준 브론즈 시뮬레이션 문제풀이 소스코드 추가 저장

---
+++ src/bakjoon/Exercise26026.java
@@ -0,0 +1,33 @@
+package bakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Exercise26026 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int lec = Integer.parseInt(br.readLine());
+        int[] machine = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
+
+        int wakeup = 0;
+        int coffee = 0;
+        for (int i = 0; i < lec; i++) {
+            if (machine[i] == 0) {
+                if (coffee > 0) {
+                    coffee--;
+                    wakeup++;
+                }
+            } else {
+                wakeup++;
+                if (coffee > 0) {
+                    coffee--;
+                }
+                coffee = Math.min(coffee+2, 2);
+            }
+        }
+        System.out.println(wakeup);
+    }
+}
---
+++ src/bakjoon/Exercise5566.java
@@ -0,0 +1,41 @@
+package bakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Exercise5566 {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] board = initArray(n);
+        int[] dice = initArray(m);
+        int now = 0, answer = 0;
+        for (int i = 0; i < m; i++) {
+            now += dice[i];
+            if (now >= n-1) {
+                answer = i+1;
+                break;
+            }
+            now += board[now];
+            if (now >= n-1) {
+                answer = i+1;
+                break;
+            }
+        }
+        System.out.println(answer);
+    }
+
+    public static int[] initArray(int len) throws IOException {
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        return arr;
+    }
+}
