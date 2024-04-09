f8ee7b816a5f582dbf63da2b333c07c3f8b7b893
Hyeonbin2379 <hyeonbin2379@gmail.com>
2024-04-09 21:30:13 +0900

    백준 브론즈 문제풀이 코드 추가

---
+++ src/bakjoon/Exercise10426.java
@@ -0,0 +1,27 @@
+package bakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.StringTokenizer;
+
+public class Exercise10426 {
+
+    public static void main(String[] args) throws IOException, ParseException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        SimpleDateFormat dtFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = dtFormat.parse(st.nextToken());
+        int days = Integer.parseInt(st.nextToken());
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.DATE, days-1);
+        System.out.println(dtFormat.format(cal.getTime()));
+    }
+}
---
+++ src/bakjoon/Exercise8805.java
@@ -0,0 +1,52 @@
+package bakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Exercise8805 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int testCase = Integer.parseInt(br.readLine());
+        for (int i = 0; i < testCase; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+
+            int[] strict = new int[n];
+            if (n == 1) {
+                sb.append(k).append("\n");
+                continue;
+            }
+
+            boolean high = false;
+            int index = 0;
+            for (int j = 1; j <= k; j++) {
+                if (index == 0) {
+                    if (high) {
+                        high = false;
+                    }
+                    strict[index++] += 1;
+                } else if (index == n-1) {
+                    if (!high) {
+                        high = true;
+                    }
+                    strict[index--] += 1;
+                } else {
+                    if (high) {
+                        strict[index--] += 1;
+                    } else {
+                        strict[index++] += 1;
+                    }
+                }
+            }
+            Arrays.stream(strict).forEach(value -> sb.append(value).append(" "));
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
