3b15f362c1f3de01bc4c8fa74426d4c51740244e
Hyeonbin2379 <hyeonbin2379@gmail.com>
2024-05-04 00:07:50 +0900

    백준 문제풀이 코드 추가

---
+++ src/bakjoon/Exercise6979.java
@@ -0,0 +1,27 @@
+package bakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Exercise6979 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCase = Integer.parseInt(br.readLine());
+        for (int i = 0; i < testCase; i++) {
+            int n = Integer.parseInt(br.readLine());
+            int[] seq1 = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int[] seq2 = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int maxDist = 0;
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    int dist = (k >= j && seq2[k] >= seq1[j]) ? k-j : 0;
+                    maxDist = Math.max(dist, maxDist);
+                }
+            }
+            System.out.printf("The maximum distance is %d\n\n", maxDist);
+        }
+    }
+}
---
+++ src/bakjoon/Exercise9151.java
@@ -0,0 +1,36 @@
+package bakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Exercise9151 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                break;
+            }
+
+            int result = 0;
+            for (int i = 1; i*i*i <= num; i++) {
+                result = Math.max(result, i*i*i);
+            }
+            for (int i = 1; i*(i+1)*(i+2)/6 <= num; i++) {
+                result = Math.max(result, i*(i+1)*(i+2)/6);
+            }
+            for (int i = 1; i*i*i <= num; i++) {
+                for (int j = 1; j*(j+1)*(j+2)/6 <= num; j++) {
+                    int temp = i*i*i + j*(j+1)*(j+2)/6;
+                    if (temp > num) {
+                        break;
+                    }
+                    result = Math.max(result, temp);
+                }
+            }
+            System.out.println(result);
+        }
+    }
+}
