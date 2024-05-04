8c4bbec8549db2a4cd3d0558b62f573d7ac25330
Hyeonbin2379 <hyeonbin2379@gmail.com>
2024-05-05 02:50:39 +0900

    백준 문제풀이 코드 추가

---
+++ src/bakjoon/Exercise30236.java
@@ -0,0 +1,32 @@
+package bakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class Exercise30236 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCase = Integer.parseInt(br.readLine());
+        for (int i = 0; i < testCase; i++) {
+            int n = Integer.parseInt(br.readLine());
+            int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+            int[] temp = IntStream.rangeClosed(1, n).toArray();
+            for (int j = 0; j < n; j++) {
+                if (temp[j] == arr[j]) {
+                    temp[j]++;
+                }
+                for (int k = j+1; k < n; k++) {
+                    if (temp[k-1] >= temp[k]) {
+                        temp[k] += (temp[k-1]-temp[k]) + 1;
+                    }
+                }
+            }
+            System.out.println(temp[n-1]);
+        }
+    }
+}
---
+++ src/bakjoon/Exercise9558.java
@@ -0,0 +1,34 @@
+package bakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Exercise9558 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCase = Integer.parseInt(br.readLine());
+        for (int i = 0; i < testCase; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int[] num1 = new int[n];
+            for (int j = 0; j < n; j++) {
+                num1[j] = Integer.parseInt(st.nextToken());
+            }
+
+            st = new StringTokenizer(br.readLine());
+            int m = Integer.parseInt(st.nextToken());
+            int min = Integer.MAX_VALUE;
+            for (int j = 0; j < m; j++) {
+                int num2 = Integer.parseInt(st.nextToken());
+                for (int k = 0; k < num1.length; k++) {
+                    min = Math.min(Math.abs(num2-num1[j]), min);
+                }
+            }
+
+            System.out.println(min);
+        }
+    }
+}
