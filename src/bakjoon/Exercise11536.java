c584d77b905a324b84138271d5423d57b45a1c12
Hyeonbin2379 <hyeonbin2379@gmail.com>
2024-05-09 06:24:40 +0900

    백준 문제풀이 코드 추가

---
+++ src/bakjoon/Exercise11536.java
@@ -0,0 +1,34 @@
+package bakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Exercise11536 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        List<String> increase = new ArrayList<>();
+        List<String> decrease = new ArrayList<>();
+        List<String> input = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String name = br.readLine();
+            input.add(name);
+            increase.add(name);
+            decrease.add(name);
+        }
+        increase.sort(null);
+        decrease.sort(Comparator.reverseOrder());
+
+        if (!input.equals(increase) && !input.equals(decrease)) {
+            System.out.println("NEITHER");
+        } else {
+            System.out.println(input.equals(increase) ? "INCREASING" : "DECREASING");
+        }
+    }
+}
