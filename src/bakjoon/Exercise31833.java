eda7534e0eebf9f35dcd00c214f7901f0baae493
Hyeonbin2379 <hyeonbin2379@gmail.com>
2024-05-15 13:56:54 +0900

    백준 문제풀이 코드 추가

---
+++ src/bakjoon/Exercise31833.java
@@ -0,0 +1,19 @@
+package bakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Exercise31833 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String str1 = String.join("", br.readLine().split(" "));
+        BigInteger num1 = new BigInteger(str1);
+        String str2 = String.join("", br.readLine().split(" "));
+        BigInteger num2 = new BigInteger(str2);
+        System.out.println(num1.min(num2));
+    }
+}
