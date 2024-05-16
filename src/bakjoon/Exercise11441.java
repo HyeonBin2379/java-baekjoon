a70b8da4ecadfae44e665c4fd7b8762bacc38639
Hyeonbin2379 <hyeonbin2379@gmail.com>
2024-05-16 10:41:24 +0900

    백준 문제풀이 코드 저장

---
+++ src/bakjoon/Exercise11441.java
@@ -0,0 +1,27 @@
+package bakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Exercise11441 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long[] prefixSum = new long[n+1];
+        for (int i = 1; i <= n; i++) {
+            prefixSum[i] = prefixSum[i-1] + Integer.parseInt(st.nextToken());
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            System.out.println(prefixSum[end]-prefixSum[start-1]);
+        }
+    }
+}
---
+++ src/bakjoon/Exercise11899.java
@@ -0,0 +1,30 @@
+package bakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Exercise11899 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] input = br.readLine().toCharArray();
+        Stack<Character> stack = new Stack<>();
+        int answer = 0;
+        for (char p : input) {
+            switch (p) {
+                case '(' -> stack.push(p);
+                case ')' -> {
+                    if (stack.isEmpty() || stack.peek() != '(') {
+                        answer++;
+                    } else {
+                        stack.pop();
+                    }
+                }
+            }
+        }
+        answer += stack.isEmpty() ? 0 : stack.size();
+        System.out.println(answer);
+    }
+}
