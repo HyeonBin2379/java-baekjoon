23c72fe610dedcd0e2098e1ae003e05e4fa5b206
Hyeonbin2379 <hyeonbin2379@gmail.com>
2024-05-25 12:56:24 +0900

    백준 문제풀이 코드 추가

---
+++ src/bakjoon/Exercise20551.java
@@ -0,0 +1,36 @@
+package bakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+
+public class Exercise20551 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
+
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(array);
+
+        Map<Integer, Integer> sorted = new TreeMap<>();
+        for (int i = 0; i < n; i++) {
+            if (!sorted.containsKey(array[i])) {
+                sorted.put(array[i], i);
+            }
+        }
+        for (int i = 0; i < q; i++) {
+            int num = Integer.parseInt(br.readLine());
+            System.out.println(sorted.getOrDefault(num, -1));
+        }
+    }
+}
---
+++ src/bakjoon/Exercise2422.java
@@ -0,0 +1,42 @@
+package bakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Exercise2422 {
+
+    private static final boolean[][] notRecommended = new boolean[201][201];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            notRecommended[a][b] = true;
+            notRecommended[b][a] = true;
+        }
+
+        int answer = 0;
+        for (int a = 1; a <= n; a++) {
+            for (int b = a+1; b <= n; b++) {
+                if (notRecommended[a][b]) {
+                    continue;
+                }
+
+                for (int c = b+1; c <= n; c++) {
+                    if (notRecommended[b][c] || notRecommended[c][a]) {
+                        continue;
+                    }
+                    answer++;
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+}
