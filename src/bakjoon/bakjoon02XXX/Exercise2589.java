ae02df092f1286699bd1d87c63c1c89eedec58b2
Hyeonbin2379 <hyeonbin2379@gmail.com>
2024-07-21 22:03:43 +0900

    백준 문제풀이 코드 추가

---
+++ src/bakjoon/bakjoon02XXX/Exercise2589.java
@@ -0,0 +1,61 @@
+package bakjoon.bakjoon02XXX;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Exercise2589 {
+
+    private static int n, m, answer;
+    private static char[][] map;
+    private static boolean[][] visited;
+    private static final int[][] move = {{-1, 0}, {0, 1}, {0, -1}, {1, 0}};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new char[n][m];
+        for (int i = 0; i < n; i++) {
+            map[i] = br.readLine().toCharArray();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (map[i][j] == 'L') {
+                    visited = new boolean[n][m];
+                    bfs(i, j);
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+
+    private static void bfs(int r, int c) {
+        Queue<Integer[]> queue = new LinkedList<>();
+        visited[r][c] = true;
+        queue.offer(new Integer[]{r, c, 0});
+
+        while (!queue.isEmpty()) {
+            Integer[] now = queue.poll();
+
+            for (int[] ints : move) {
+                int nr = now[0] + ints[0];
+                int nc = now[1] + ints[1];
+                int nDist = now[2] + 1;
+
+                if (nr < 0 || nr >= n || nc < 0 || nc >= m) {
+                    continue;
+                }
+                if (!visited[nr][nc] && map[nr][nc] == 'L') {
+                    visited[nr][nc] = true;
+                    queue.offer(new Integer[]{nr, nc, nDist});
+                    answer = Math.max(answer, nDist);
+                }
+            }
+        }
+    }
+}
