5792129ef2a07fb8cfa01be6c733f1fe64466b47
Hyeonbin2379 <hyeonbin2379@gmail.com>
2024-08-23 03:42:36 +0900

    백준 문제풀이 코드 추가

---
+++ src/bakjoon/bakjoon31XXX/Exercise31245.java
@@ -0,0 +1,21 @@
+package bakjoon.bakjoon31XXX;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Exercise31245 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] num = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        StringBuilder sb = new StringBuilder(Integer.toString(num[0]));
+        for (int i = 1; i < 3; i++) {
+            if (num[i-1] % 10 == num[i]/10) {
+                sb.append("'").append(num[i] % 10);
+            } else {
+                sb.append(num[i]);
+            }
+        }
+        System.out.println(sb);
+    }
+}
