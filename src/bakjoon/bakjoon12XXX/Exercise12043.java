c251607f13ed0f27d6a660aedac10dcfefa725d2
Hyeonbin2379 <hyeonbin2379@gmail.com>
2024-08-24 22:40:22 +0900

    백준 문제풀이 코드 추가

---
+++ src/bakjoon/bakjoon12XXX/Exercise12043.java
@@ -0,0 +1,36 @@
+package bakjoon.bakjoon12XXX;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Exercise12043 {
+
+    private static final long MOD = (int)1e9 + 7;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int testcase = Integer.parseInt(reader.readLine());
+
+        for (int t = 1; t <= testcase; t++) {
+            String str = reader.readLine();
+            long ans = 1;
+
+            for (int i = 0; i < str.length(); i++) {
+                Set<Character> uniqueChars = new HashSet<>();
+                if (i > 0) {
+                    uniqueChars.add(str.charAt(i - 1)); // 이전 글자 추가
+                }
+                uniqueChars.add(str.charAt(i)); // 현재 글자 추가
+                if (i+1 < str.length()) {
+                    uniqueChars.add(str.charAt(i + 1)); // 다음 글자 추가
+                }
+                ans = (ans * uniqueChars.size()) % MOD; // 가능한 글자의 수를 곱함
+            }
+
+            System.out.printf("Case #%d: %d%n", t, ans);
+        }
+    }
+}
---
+++ src/bakjoon/bakjoon14XXX/Exercise14315.java
@@ -0,0 +1,23 @@
+package bakjoon.bakjoon14XXX;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Exercise14315 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            long l = Long.parseLong(st.nextToken());
+            long r = Long.parseLong(st.nextToken());
+            long p = Math.min(l, r);
+            sb.append(String.format("Case #%d: %d\n", i, p*(p+1)/2));
+        }
+        System.out.print(sb);
+    }
+}
