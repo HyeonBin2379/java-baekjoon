b591f59ae534771fff55ef1abe66e30a51256f35
Hyeonbin2379 <hyeonbin2379@gmail.com>
2024-09-01 22:44:44 +0900

    백준 문제풀이 코드 추가

---
+++ src/bakjoon/bakjoon32XXX/Exercise32184.java
@@ -0,0 +1,18 @@
+package bakjoon.bakjoon32XXX;
+
+import java.util.Scanner;
+
+public class Exercise32184 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a % 2 == b % 2) {
+            System.out.println((b-a+1)/2 + 1);
+        } else {
+            System.out.println(a % 2 == 1 ? (b-a+1)/2 : (b-a+1)/2+1);
+        }
+    }
+}
