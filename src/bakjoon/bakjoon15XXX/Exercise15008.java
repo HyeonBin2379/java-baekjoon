fd3e5dda00fbaa6422d04169026acabed4b66b33
Hyeonbin2379 <hyeonbin2379@gmail.com>
2024-09-04 01:27:52 +0900

    백준 문제풀이 코드 추가

---
+++ src/bakjoon/bakjoon15XXX/Exercise15008.java
@@ -0,0 +1,29 @@
+package bakjoon.bakjoon15XXX;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Exercise15008 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Integer[] num = Arrays.stream(br.readLine().split(" "))
+                .map(Integer::parseInt)
+                .sorted(Comparator.reverseOrder())
+                .toArray(Integer[]::new);
+
+        int alice = 0, bob = 0;
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                alice += num[i];
+            } else {
+                bob += num[i];
+            }
+        }
+        System.out.println(alice + " " + bob);
+    }
+}
---
+++ src/bakjoon/bakjoon15XXX/Exercise15600.java
@@ -0,0 +1,12 @@
+package bakjoon.bakjoon15XXX;
+
+import java.util.Scanner;
+
+public class Exercise15600 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(Math.max(n-2, 1));
+    }
+}
