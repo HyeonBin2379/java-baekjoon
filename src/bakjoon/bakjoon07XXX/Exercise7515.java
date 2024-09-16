301ce00eee136d68921ea5a0863bc6cd70b9c255
Hyeonbin2379 <hyeonbin2379@gmail.com>
2024-09-17 08:36:39 +0900

    백준 문제풀이 코드 추가

---
+++ src/bakjoon/bakjoon07XXX/Exercise7515.java
@@ -0,0 +1,23 @@
+package bakjoon.bakjoon07XXX;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Exercise7515 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int testcase = Integer.parseInt(br.readLine());
+        int[] dp = new int[41];
+        for (int i = 0; i < dp.length; i++) {
+            dp[i] = (i < 2) ? i+1 : dp[i-1] + dp[i-2];
+        }
+        for (int i = 1; i <= testcase; i++) {
+            int n = Integer.parseInt(br.readLine());
+            sb.append(String.format("Scenario %d: %d\n\n", i, dp[n]));
+        }
+        System.out.print(sb);
+    }
+}
